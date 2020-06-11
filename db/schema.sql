67d314f73c88afcdb253f33c5e80b2de34d94949
Kyle Cooper <kcooper89.kc@gmail.com>
2020-06-11 15:55:01 -0400

    6/11/20 2

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,13 @@ USE burgers_db;
 
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  burgerName VARCHAR(20) NOT NULL,
+  burgerName VARCHAR (20) NOT NULL,
   devoured BOOLEAN DEFAULT FALSE
 );
+
+INSERT INTO burgers (burgerName) VALUES ('Giantic Burger');
+INSERT INTO burgers (burgerName) VALUES ('Tiny Burger');
+INSERT INTO burgers (burgerName, devoured) VALUES ('Square Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Circule Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Triangle  Burger', true);
+INSERT INTO burgers (burgerName) VALUES ('Octagon Burger');
