5389c29701b4dd95f46a32a85bbfc97d6fc8a4a3
Kyle Cooper <kcooper89.kc@gmail.com>
2020-07-16 18:17:56 -0400

    jaws final

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,16 @@ var mysql = require("mysql");
 var connection;
 
 if (!process.env.JAWSDB_URL) {
-  connection = mysql.createConnection({
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
     database: "burgers_db"
-  });
-} else {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
+ }); 
+};
 
 // Make connection.
 connection.connect(function(err) {
